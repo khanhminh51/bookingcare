ea5f11e9527b8c98bf012bd48d1aa3117980d834
Minh PK <minh.phambku51@hcmut.edu.vn>
2023-07-04 14:01:23 +0700

    bulk create data and get schedule data

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -29,7 +29,7 @@ let getAllDoctors = async (req, res) => {
     }
 }
 
-let postInforDoctor = async(req,res) =>{
+let postInforDoctor = async (req, res) => {
     try {
         let response = await doctorService.saveDetailInforDoctor(req.body);
         return res.status(200).json(response)
@@ -43,11 +43,11 @@ let postInforDoctor = async(req,res) =>{
 
 }
 
-let getDetailDoctorById = async(req, res) => {
+let getDetailDoctorById = async (req, res) => {
     try {
         let infor = await doctorService.getDetailDoctorById(req.query.id)
         return res.status(200).json(infor)
-        
+
     } catch (e) {
         console.log(e)
         return res.status(200).json({
@@ -57,9 +57,40 @@ let getDetailDoctorById = async(req, res) => {
     }
 
 }
+
+let bulkCreateSchedule = async (req, res) => {
+    try {
+        let infor = await doctorService.bulkCreateSchedule(req.body)
+        return res.status(200).json({
+            infor
+        })
+    } catch (e) {
+        console.log(e)
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from server'
+        })
+    }
+
+}
+
+let getScheduleByDate = async (req, res) => {
+    try {
+        let infor = await doctorService.getScheduleByDate(req.query.doctorId, req.query.date);
+        return res.status(200).json(infor)
+    } catch (e) {
+        console.log(e)
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from server'
+        })
+    }
+}
 module.exports = {
     getTopDoctorHome: getTopDoctorHome,
     getAllDoctors: getAllDoctors,
     postInforDoctor: postInforDoctor,
-    getDetailDoctorById: getDetailDoctorById
+    getDetailDoctorById: getDetailDoctorById,
+    bulkCreateSchedule: bulkCreateSchedule,
+    getScheduleByDate: getScheduleByDate
 }
