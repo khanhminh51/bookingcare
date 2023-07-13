6b4bffbd0fd94e3caa951d835bd07d49c6e64ee7
Minh PK <minh.phambku51@hcmut.edu.vn>
2023-07-13 11:41:36 +0700

    complete medical facility component

---
+++ src/services/clinicService.js
@@ -0,0 +1,100 @@
+import db from "../../models/index";
+// const db = require('../../models');
+
+
+let createClinic = (data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!data.name || !data.address || !data.imageBase64
+                || !data.descriptionMarkdown || !data.descriptionHTML) {
+                resolve({
+                    errCode: 1,
+                    errMessage: 'Missing required parameter!'
+                })
+            }
+            else {
+                await db.Clinic.create({
+                    name: data.name,
+                    address: data.address,
+                    descriptionMarkdown: data.descriptionMarkdown,
+                    descriptionHTML: data.descriptionHTML,
+                    image: data.imageBase64
+                })
+                resolve({
+                    errCode: 0,
+                    errMessage: 'OK'
+                })
+            }
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+let getAllClinic = (data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let data = await db.Clinic.findAll({
+
+            });
+            if (data && data.length > 0) {
+                data.map(item => {
+                    // item.image = new Buffer(item.image, 'base64').toString('binary');
+                    item.image = Buffer.from(item.image, 'base64').toString('binary');
+                    return item;
+                })
+            }
+            resolve({
+                errMessage: 'OK',
+                errCode: 0,
+                data
+            });
+
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+let getDetailClinicById = (inputId) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!inputId) {
+                resolve({
+                    errCode: 1,
+                    errMessage: 'Missing parameter'
+                })
+            }
+            else {
+                let data = await db.Clinic.findOne({
+                    where: { id: inputId },
+                    attributes: ['name', 'address', 'descriptionHTML', 'descriptionMarkdown'],
+                })
+                if (data) {
+                    let doctorClinic = [];
+
+                    doctorClinic = await db.Doctor_Infor.findAll({
+                        where: { clinicId: inputId },
+                        attributes: ['doctorId', 'provinceId'],
+                    })
+                    data.doctorClinic = doctorClinic;
+                }
+                else data = {}
+                resolve({
+                    errMessage: 'OK',
+                    errCode: 0,
+                    data
+                });
+            }
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+
+
+
+module.exports = {
+    createClinic: createClinic,
+    getAllClinic: getAllClinic,
+    getDetailClinicById: getDetailClinicById,
+
+}
